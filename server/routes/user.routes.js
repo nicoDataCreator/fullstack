4352c1f8e38efc741f39dd62e1e701a7dc622b8a
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-05-22 16:27:28 +0200

    server/create passport login with google

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,8 +2,13 @@ const express = require('express');
 const userController = require('../controllers/user.controller');
 const router = express.Router();
 
-// Create a new user
+// http://localhost:3000/api/user/create
+// Create a new user in the "alumnos" table (guarda en la base de datos "alumnos" el perfil total del formulario de contrato)
 router.post('/create', userController.createUser);
 
+// añadir ruta para crear usuario en la base de datos (tabla login)
+
 module.exports = router;
+
+
 
