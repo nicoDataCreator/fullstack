b6c1ac21dfc06662833e319d16da07b50176e78a
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-05-23 00:06:46 +0200

    client/create streamlit component, reduce home image size

---
+++ client/src/components/Streamlit/Streamlit.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Streamlit = () => {
+  return (
+    <div>
+      <iframe className="chatty"
+        src="https://chatybe.streamlit.app/?embed=true"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Streamlit;
