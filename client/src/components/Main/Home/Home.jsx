5105ef15ac56cff551cdd223a50a4e1af291623c
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-05-23 12:31:08 +0200

    client/fix _home.scss, fix _nav.scss styles

--- client/src/components/Main/Home/Home.jsx
+++ client/src/components/Main/Home/Home.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import fotoprincipal from "../../../assets/estudiantessonriendo.jpg"
+import Contact from '../Contact/Contact'
 
 const Home = () => {
+  const contactRef = useRef(null);
+
+  const handleScrollToContact = () => {
+    if (contactRef.current) {
+      contactRef.current.scrollIntoView({ behavior: "smooth"});
+    }
+  }
   return (
     <>
-  <section className="home">
-    <img className="principal" src={fotoprincipal}></img>
-    <article className="textofondo">
-      <h3 className="primerafrase" >En Beyond education te acompañamos en cada decisión que tomes,</h3>
-      <h3 className="segundafrase" >elige la orientación que necesitas</h3>
-    </article>
-  </section>
-  </>
-)};
+      <section className="home">
+        <img className="principal" src={fotoprincipal}></img>
+
+        <article className="textofondo">
+          <h3 className="primerafrase" >En Beyond education te acompañamos en cada decisión que tomes,</h3>
+          <h3 className="segundafrase" >elige la orientación que necesitas</h3>
+        </article>
+
+        <article ref={contactRef}>
+          <Contact />
+        </article>
+      </section>
+    </>
+  )
+};
 
 export default Home;
