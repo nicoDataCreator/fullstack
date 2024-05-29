b8a070571b1136736c9420d8c6066089cb8e85be
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-05-29 12:19:27 +0200

    cleanup passport on server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 process.on('warning', e => console.warn(e.stack));
 
-// Requirements: express, pg
+// Requirements: express, express-session, cors
 const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
+
+// Requirements: routes
+const userRoutes = require("./routes/user.routes");
+const passport = require("passport");
 require('../server/config/auth.js')
 
 const isLoggedIn = (req, res, next) => {
@@ -13,11 +17,6 @@ const isLoggedIn = (req, res, next) => {
 const app = express();
 const port = 3000;
 
-
-// Requirements: routes
-const userRoutes = require("./routes/user.routes");
-const passport = require("passport");
-
 // Middlewares
 app.use(cors());
 app.use(express.json());
@@ -26,29 +25,20 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-/* ----- WEB ROUTES ----- */
-// http://localhost:3000/
-// Home page
 app.get('/', (req, res) => {
   res.status(200).send("Hello World!");
 });
 
-// http://localhost:3000/auth
-// Page shows a button with a link that redirects to '/auth/google'
 app.get('/auth', (req, res) => {
   res.send('<a href="/auth/google">Authenticate with google</a>');
 });
 
-// http://localhost:3000/auth/google
-// Shows user selection through Google and allows Sign-up and Sign-in
 app.get('/auth/google',
   passport.authenticate('google', {
     scope: ['email', 'profile']
   })
 );
 
-// http://localhost:3000/google/callback
-// Handles whether Sign-in was successful or not to access the protected route
 app.get('/google/callback',
   passport.authenticate('google', {
     successRedirect: '/protected',
@@ -56,20 +46,14 @@ app.get('/google/callback',
   })
 );
 
-// http://localhost:3000/auth/failure
-// Redirects the user if the sign-in was un-successful
 app.get('/auth/failure', (req, res) => {
   res.send('Authentication failed');
 })
 
-
-// http://localhost:3000/protected
-// Redirects the user if the sign-in was successful
 app.get('/protected', isLoggedIn, (req, res) => {
   res.send('Hello User');
 });
 
-/* ----- API ROUTES ----- */
 app.use('/api/user', userRoutes);
 
 const server = app.listen(port, () => {
