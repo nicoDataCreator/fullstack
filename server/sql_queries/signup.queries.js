e3e50815cb03e5175c6961a741905b7cd6e52a61
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-05-29 10:45:50 +0200

    server/fix createUser query for table alumnos

--- server/sql_queries/signup.queries.js
+++ server/sql_queries/signup.queries.js
@@ -2,7 +2,7 @@ const signUpQueries = {
     // se insertan todos los datos del cliente en la bbdd
     // quitar la ñ por si da problemas?
     createUser: `
-    INSERT INTO log_in(id_alumno, email, password, log_in_status)
+    INSERT INTO alumnos(id_alumno, email, password, log_in_status)
     VALUES ($1, $2, $3, $4)
     `,
     getUser: `
